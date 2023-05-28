1760d84b90a11e4baf10c41c297b413c05f68f8d
Muhammad Arbaz Dogar <arbazdogar20@gmail.com>
2023-05-28 22:30:50 +0500

    Delete routes directory

--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const {register, login, logout} = require('../controllers/auth.controller');
-
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
-
-module.exports = router;
